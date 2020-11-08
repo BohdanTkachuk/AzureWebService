f45743e4cad7082917ff8014d9dd8de0f8f1676d
BohdanTkachuk <49440230+BohdanTkachuk@users.noreply.github.com>
2020-11-08 16:22:43 +0100

    Update vars.tf
    Added default values, added validation for number of vm

--- vars.tf
+++ vars.tf
@@ -1,21 +1,30 @@
 variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
+  default = "newProject"
 }
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
+  default = "germanywestcentral"
 }
 
 variable "user" {
   description = "The username for authentication"
+  default = "newUser"
 }
 
 variable "password" {
   description = "The password for authentication"
+  default = "P@ssword"
 }
 
 variable "instances"{
 	description = "The number of instances that will be created"
+	default = 2
+	validation {
+		condition = var.instances < 6	
+		error_message = "The number of instances must be smaller or equal than 5."
+	}
 }
 
 variable "tenant_id" {
